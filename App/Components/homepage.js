0c0896255501710aec08779f4ec09731e9fbd033
Aishwarya Ramanathan <cutieice@gmail.com>
2020-08-02 23:47:39 -0400

    added a small description of me

--- App/Components/homepage.js
+++ App/Components/homepage.js
@@ -10,6 +10,7 @@ class Homepage extends React.Component {
                 <ul className="home-list">
                      <li>MMath Candidate at University of Waterloo</li>
                 </ul>
+                <p>Hiya! I’m Aishwarya. You can call me Ash if that’s easier. I enjoy crocheting, painting, and playing football.</p>
                 <img className="pp" src="./images/pp.jpg"/>
             </div>
         );
