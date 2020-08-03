725b5e4daa3a8f9c5aced225daed0be3fda51d67
Aishwarya Ramanathan <cutieice@gmail.com>
2020-08-03 16:51:51 -0400

    Added CV pdf

--- App/Components/homepage.jsx
+++ App/Components/homepage.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import Nav from "./nav.jsx";
 
 class Homepage extends React.Component {
     render() {
         return (
-                    <div className="homepage">
-                        <h1>Aishwarya Ramanathan</h1>
-                        <ul className="home-list">
-                             <li>MMath Candidate at University of Waterloo</li>
-                        </ul>
-                        <p>Hiya! I’m Aishwarya. You can call me Ash if that’s easier. I enjoy crocheting, painting, and playing football.</p>
-                        <img className="pp" src="./images/pp.jpg"/>
-                    </div>
+            <div className="homepage">
+                <h1>Aishwarya Ramanathan</h1>
+                <ul className="home-list">
+                     <li>MMath Candidate at the University of Waterloo</li>
+                </ul>
+                <p>Hiya! I’m Aishwarya. You can call me Ash if that’s easier. I enjoy crocheting, painting, and playing football.</p>
+                <img className="pp" src="./images/pp.jpg"/>
+                <a href={"./files/CV2020.pdf"}>CV</a>
+            </div>
         );
     }
 }
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p deploy 
-mv app.js *.org *.html *.css images/ deploy
+mv app.js *.org *.html *.css images/ files/ deploy
